940063d1a8a6a086343cb46fe3426e811f9a93f3
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2022-12-30 01:33:11 +0100

    I finished the JavaScript in one night, unbelievable😩

---
+++ script.js
@@ -0,0 +1,64 @@
+let addTaskButton = document.getElementById("addTask")
+let newTask = document.querySelector("#taske")
+let editButton = document.getElementsByClassName("edit-button")
+let deleteButton = document.getElementsByClassName("delete")
+let input = document.getElementsByClassName("task")
+let ul = document.getElementsByTagName("ul")[0]
+
+
+
+
+function addTask() {
+    let newTaskValue = newTask.value
+    if(newTaskValue != ""){
+        newTask.value = "";
+        const li = document.createElement("li");
+
+        let addInput = document.createElement("input")
+        addInput.type = "text"
+        addInput.classList.add("task")
+        addInput.value = newTaskValue
+        addInput.setAttribute("readonly", "readonly")
+        
+        const span1 = document.createElement("span")
+        span1.classList.add("edit-button")
+        span1.innerHTML = "Edit"
+
+        const span2 = document.createElement("span")
+        span2.classList.add("delete")
+        span2.innerHTML = "Delete"
+
+
+        li.appendChild(addInput)
+        li.appendChild(span1)
+        li.appendChild(span2)
+
+        ul.appendChild(li)
+
+        span2.addEventListener("click", () =>{
+            this.parentElement.style.display='none';
+        })
+
+        span1.addEventListener("click", ()=>{
+            if (span1.innerHTML !== "Save"){
+                addInput.removeAttribute("readonly");
+                addInput.focus()
+                span1.innerHTML= "Save"
+            }
+            else{
+                addInput.setAttribute("readonly", "readonly")
+                span1.innerHTML= "Edit"
+            }
+        })
+    }
+
+    else if(newTaskValue == ""){
+        alert("Please add a task")
+    }
+}
+
+
+
+
+
+addTaskButton.addEventListener("click", addTask)
